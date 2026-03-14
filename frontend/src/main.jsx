4259d6318ba5a7580095a87e121e88b173401668
Nguyen Thanh Hao <nguyenthanhhao@PharaohMac.local>
2026-03-14 15:48:28 +0700

    hhe

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.jsx';
+import { createHashRouter, RouterProvider } from 'react-router-dom'; import App from './App.jsx';
 import Shop from './pages/Shop.jsx';
 import Success from './pages/Success.jsx';
 import Cancel from './pages/Cancel.jsx';
@@ -22,10 +21,10 @@ import AdminProductHistory from './pages/AdminProductHistory.jsx';
 import NotFound from './pages/NotFound.jsx';
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
-    element: <App/>,
+    element: <App />,
     children: [
       { index: true, element: <Shop /> },
       { path: 'success', element: <Success /> },
@@ -33,22 +32,22 @@ const router = createBrowserRouter([
       { path: 'payment', element: <Payment /> },
       { path: 'products/:id', element: <ProductDetail /> },
       { path: 'user/login', element: <UserLogin /> },
-      { path: 'user/register', element: <UserRegister/> },
-      { 
-        path: 'user/dashboard', 
+      { path: 'user/register', element: <UserRegister /> },
+      {
+        path: 'user/dashboard',
         element: (
           <ProtectedRoute>
-            <UserDashboard/>
+            <UserDashboard />
           </ProtectedRoute>
-        ) 
+        )
       },
       { path: 'admin', element: <AdminLogin /> },
-      { path: 'admin/users', element: <AdminUsers/> },
+      { path: 'admin/users', element: <AdminUsers /> },
       { path: 'admin/products', element: <AdminProducts /> },
       { path: 'admin/products/addproduct', element: <AdminAddProduct /> },
       { path: 'admin/products/edit/:id', element: <AdminEditProduct /> },
       { path: 'admin/products/:id/history', element: <AdminProductHistory /> },
-      { path: 'admin/transactions', element: <AdminTransactionsPage/> },
+      { path: 'admin/transactions', element: <AdminTransactionsPage /> },
       { path: 'admin/failedlogin', element: <AdminFailedLogin /> },
       { path: '404', element: <NotFound /> }
     ]
